a168047ed6efa21a5472d5fbaa8c35db2a578dc8
Guillermo Jiménez <guillermo_jimenez@outlook.com>
2016-06-26 17:16:18 -0500

    Remove writing page. Put respective card on main

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@
     event.preventDefault();
     $('.coding-card').slideToggle(250);
   });
+  $('.writing-card').toggle(100);
+  $('#writing').click(function (event) {
+    event.preventDefault();
+    $('.writing-card').slideToggle(250);
+  });
 }());
