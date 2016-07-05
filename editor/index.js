fc7878ef683d808de6ef7c27dce4c977fec23542
Guillermo Jiménez <guillermo_jimenez@outlook.com>
2016-07-05 09:12:04 -0500

    Create blog posts editor.

--- editor/index.js
+++ editor/index.js
@@ -8,17 +8,20 @@
   let app = express();
 
   let blog_posts = [];
+  const filepath = '../blog-posts.json';
 
   (function() {
-    //read blog posts file
-    const filepath = '../blog-posts.json';
     try {
-      blog_posts = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+      blog_posts = JSON.parse(fs.readFileSync(filepath));
     } catch (e) {
       fs.closeSync(fs.openSync(filepath, 'w'));
     }
   }());
 
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
   app.use(morgan('combined'));
   app.use(bodyParser.urlencoded({
     extended: true
@@ -31,10 +34,21 @@
       {
         title: req.body.title,
         date: new Date(),
+        image: req.body.image,
         content: req.body.content
       });
+    fs.writeFileSync(filepath, JSON.stringify(blog_posts));
     res.send('Blog post added');
   });
+  app.get('/images', function (req, res) {
+    fs.readdir('../img/blog/', function(err, items) {
+      if (err) {
+        console.log(err);
+        res.status(404).send('Couldn\'t perform query');
+      }
+      res.send(items);
+    });
+  })
 
   app.listen(3000, () => {console.log('Listening on port 3000')});
 }());
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function() {
+($(function() {
   'use strict';
   //code
 
@@ -14,8 +14,53 @@
     $('.writing-card').slideToggle(250);
   });
 
+  //Dogs
+  if (typeof moment !== 'undefined') {
+    $.ajax('/blog-posts.json')
+      .done(function (data) {
+        for (var i = data.length-1; i >= 0; i--) {
+          var date_real = moment(data[i].date).format('MMM Do, YYYY');
+          var date_display = moment(data[i].date).fromNow();
+          $('.blog-container').append(
+            '<article>'+
+              '<div class="row">'+
+                '<div class="col-xs-12">'+
+                  '<div class="post-title">'+
+                    '<h1>'+data[i].title+'</h1>'+
+                  '</div>'+
+                  '<div class="post-data">'+
+                    '<author>Guillermo Jiménez</author>'+' &#183; '+
+                    '<date title="'+date_real+'">'+date_display+'</date>'+
+                  '</div>'+
+                  '<div class="post-image">'+
+                    '<img class="img-responsive center-block" src="/img/blog/'+data[i].image+'"></img>'+
+                  '</div>'+
+                  '<div class="post-content">'+
+                    data[i].content+
+                  '</div>'+
+                '</div>'+
+              '</div>'+
+            '</article>'+
+            '<hr>'
+          );
+        }
+      });
+  }
+
   //editor
-  tinymce.init({
-    selector: '#content'
-  });
-}());
+  if (typeof tinymce !== 'undefined') {
+    $.ajax('http://0.0.0.0:3000/images')
+      .done(function (images) {
+        for (var i = 0; i < images.length; i++) {
+          $('#image').append(
+            '<option value="'+images[i]+'">'+
+              images[i] +
+            '</option>'
+          );
+        }
+      });
+    tinymce.init({
+      selector: '#content'
+    });
+  }
+}));
