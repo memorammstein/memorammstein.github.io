c7923f5a8bc225d932ded9f25eaadcf5fed93bb5
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2017-08-17 16:31:55 -0500

    Remove github-cards as it has proven unreliable. Use github link instead.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,6 @@
   //code
 
   //front-page
-  $('.coding-card').toggle(100);
-  $('#coding').click(function (event) {
-    event.preventDefault();
-    $('.coding-card').slideToggle(250);
-  });
   $('.writing-card').toggle(100);
   $('#writing').click(function (event) {
     event.preventDefault();
